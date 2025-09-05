0bb5242f18d70da4a5064163f3ce78c6b4de3eab
mskulas <mskulas@murraystate.edu>
2025-09-05 13:15:37 -0500

    finished up to level 1

--- AppLauncher.java
+++ AppLauncher.java
@@ -1,14 +1,29 @@
+import java.util.ArrayList;
+
 public class AppLauncher {
     public static void main(String[] args) {
         animalclass a = new animalclass();
-        animalclass.dog dog = a.new dog();
-        animalclass.cat cat = a.new cat();
-        animalclass.fox fox = a.new fox();
+        animalclass.dog Diesel = a.new dog("Diesel", "Boxer");
+        animalclass.dog Lucky = a.new dog("Lucky", "Chihuahua");
+        animalclass.cat Cocoamass = a.new cat("Cocoamass", "Havana Brown");
+        animalclass.fox Foxy = a.new fox("Foxy", "Red Fox"); // what do people even name foxes?
+        animalclass.orangeCat Jambo = a.new orangeCat("Jambo");
+        
+        ArrayList<animalclass.Animal> zoo = new ArrayList<animalclass.Animal>();
+        zoo.add(Diesel);
+        zoo.add(Lucky);
+        zoo.add(Cocoamass);
+        zoo.add(Foxy);
+        zoo.add(Jambo);
 
-        dog.makeSound();
-        cat.makeSound();
-        fox.makeSound();
-        cat.pounce();
-        fox.pounce();
+        System.out.println();
+        for (animalclass.Animal animal : zoo) {
+            System.out.print(animal.getName() + " the " + animal.getBreed() + " says: "); // this does result in "Jambo the orange" which is not grammatically correct, but it's funny so I'm not fixing it.
+            animal.makeSound();
+            if (animal instanceof animalclass.canPounce) {
+                ((animalclass.canPounce) animal).pounce();
+            }
+            System.out.println();
+        }
     }
 }
--- animalclass.java
+++ animalclass.java
@@ -1,32 +1,71 @@
 public class animalclass {
     abstract class Animal {
         abstract void makeSound();
+        private String name;
+        private String breed;
+        public String getBreed() {
+            return breed;
+        }
+        public void setBreed(String breed) {
+            this.breed = breed;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
     interface canPounce {
         public void pounce();
     }
 
     class dog extends Animal {
+        public dog(String name, String breed) {
+            setName(name);
+            setBreed(breed);
+        }
+        
         void makeSound() {
             System.out.println("Woof");
         }
     }   
     class cat extends Animal implements canPounce {
+        public cat(String name, String breed) {
+            setName(name);
+            setBreed(breed);
+            if (breed.equals("orange")) {
+                setBreed("Red Tabby"); // this is to avoid confusion with the orangeCat subclass. orange is not a color, it is a mindset.
+            }
+        }
         void makeSound() {
             System.out.println("Meow");
         }
         public void pounce() {
-            System.out.println("The cat pounces!");
+            System.out.println(this.getName() + " pounces!");
         }
     }
     class fox extends Animal implements canPounce{
+        public fox(String name, String breed) {
+            setName(name);
+            setBreed(breed);
+        }
         void makeSound() {
             System.out.println("Ring-ding-ding-ding-dingerdingerding"); // i don't like that copilot is predicting my jokes
         }
         public void pounce() {
-            System.out.println("The fox pounces!");
+            System.out.println(this.getName() + " pounces!");
         }
     } 
+
+    class orangeCat extends cat {
+        public orangeCat(String name) {
+            super(name, "orange"); // orange cats are always orange, no exceptions.
+        }
+        @Override public void pounce() {
+            System.out.println(this.getName() + " pounces and misses!"); // orange cats are generally more chaotic and less smart. no brain cells.
+        }
+    }
 
     
 }
